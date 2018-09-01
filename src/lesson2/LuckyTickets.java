3fb09d41fd8c471a4b74bf8f0542a0b136480d17
viktor3101 <viktorgusakivskii@gmail.com>
2018-09-01 18:04:22 +0300

    Add LuckyTickets.java

--- src/lesson2/LuckyTickets.java
+++ src/lesson2/LuckyTickets.java
@@ -5,18 +5,30 @@ public class LuckyTickets {
     public static void main(String[] args) {
         //int i = 1_000_000;
         int counter = 0;
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
-        int e = 0;
-        int f = 0;
         for (int i = 0; i < 1000000; i++) {
-            a = i / 100_000;
+            //запис числа з білету як стрічки
+            String a = String.valueOf(i);
+
+            //запис стрічки як масиву char
+            String b ="000000";
+            char[] array = b.toCharArray();
+            for (int j = 0; j < a.length(); j++) {
+                array[j + 6 - a.length()] = a.charAt(j);
+            }
+
+            //ініціалізація інт масива
+            int[] intArray = new int[6];
+            for (int j = 0; j < intArray.length; j++) {
+                intArray[j] = (int) array[j];
+            }
+
+            //провірка основної умови
+            if ((intArray[0] + intArray[1] + intArray[2]) == (intArray[3] + intArray[4] + intArray[5])) {
+                counter++;
+            }
         }
         System.out.println("Count of lucky numbers " + counter);
-
-        double ave = (counter / 1_000_000.0) * 100;
-        System.out.println("It is " + ave + "% of all tickets");
+        double proc = (counter / 1_000_000.0) * 100;
+        System.out.println("It is " + proc + "% of all tickets");
     }
 }
