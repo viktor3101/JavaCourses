5b75b0a3c568ccb63d746c219c1cf8a3ef63a379
viktor3101 <viktorgusakivskii@gmail.com>
2018-09-01 16:06:05 +0300

    Add PersonalInfo.java

---
+++ src/lesson2/PersonalInfo.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int age = sc.nextInt();
+        System.out.println("Hello, i'm " + name + ", " + age + " years old");
+
+
+        sc.close();
+    }
+}
